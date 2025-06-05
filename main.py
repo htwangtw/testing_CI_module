42c30146e0e015749f95a5a9520325598f498b2c
Hao-Ting Wang <htwangtw@gmail.com>
2025-06-05 14:36:22 -0400

    add test and correct the error

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def extract_nifti_data(img_path: str) -> np.ndarray:
 
 def threshold_data(data: np.ndarray, threshold: float) -> np.ndarray:
     """Threshold the data to only keep the voxels with a value above the threshold."""
-    thresholded_data = data[data < threshold]
+    thresholded_data = data[data > threshold]
     return thresholded_data
 
 
@@ -38,3 +38,51 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args.img_path, args.threshold)
+
+import pytest
+
+
+def create_sample_nifti(mean: float = 4.0) -> nib.Nifti1Image:
+        # Example usage with a sample NIfTI file and threshold
+    from nibabel import Nifti1Image
+    import nibabel as nib
+    import numpy as np
+    from numpy import random
+    data = random.rand(10, 10, 10)
+    data_mean = np.mean(data)
+
+    sample_data = Nifti1Image(
+        data - data_mean + mean, affine=np.eye(4)
+    )  # Create a sample NIfTI image
+    return sample_data
+
+def test_main(capsys, tmp_path):
+    """Test the main function with a sample nifti file."""
+    # Example usage with a sample NIfTI file and threshold
+    import nibabel as nib
+    sample_data = create_sample_nifti(mean=4.0)
+    sample_data_path = tmp_path / "sample.nii.gz"
+    nib.save(sample_data, sample_data_path)
+    # Example threshold value
+    
+    main(sample_data_path, 0.0)
+    captured = capsys.readouterr()
+    assert "Mean of voxels values above 0.0: 4.0\n" == captured.out
+
+
+def test_get_mean(tmp_path):
+    """Test the get_mean function."""
+    import numpy as np
+    data = np.random.rand(10)
+    data-= np.mean(data)
+    data+= 4.0
+    mean = get_mean(data)
+    np.testing.assert_almost_equal(mean, 4.0)
+
+def test_threshold_data():
+    """Test the threshold_data function."""
+    import numpy as np
+    data = np.random.rand(10) + 4.0
+    thresholded_data = threshold_data(data, 4.0)
+    assert sum(thresholded_data > 4.0) == 10
+
